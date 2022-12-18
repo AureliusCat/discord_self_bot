e7144b91458c0233a1afa4301a1c8d48a3e587ce
AureliusCat <johndrako@pm.me>
2022-12-17 17:09:44 -0900

    updated the help command

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,13 @@ const { prefix } = require(`../settings/config.json`);
 const helpM = `\`\`\`ini
 [${prefix}ping] - Replies with the latency of the bot
 [${prefix}kill] - Kills the bot process
-[${prefix}userinfo] - Replies with information about the mentioned user
-[${prefix}servinfo -m] - Replies with information about the guild, -m is members
+[${prefix}uinfo] - Replies with information about you
+[${prefix}sinfo] - Replies with information about the guild
 [${prefix}help] - Replies with this message
 [${prefix}ghost <mention>] - Ghost pings the mentioned user
-[${prefix}coinflip] - Flips a coin, heads or tails
-[${prefix}spamserver] - Spams all channels of the guild with the given message
+[${prefix}flip] - Flips a coin, heads or tails
+[${prefix}sserv start <message>] - Spams all channels of the guild with the given message
+[${prefix}sserv stop] - Stops the spam
 \`\`\``;
 
 module.exports = {
